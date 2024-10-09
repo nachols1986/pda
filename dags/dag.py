0f26466a34c8bae3e597db9b57173760a391c844
Jose Lopez Saez <ext.jlopezsaez@riotgames.com>
2024-10-08 22:29:30 -0300

    Corregidas encapsulaciones de archivos y tareas del dag

--- dags/dag.py
+++ dags/dag.py
@@ -3,42 +3,36 @@ from airflow.operators.python import PythonOperator
 from datetime import timedelta, datetime
 import os
 
+from src.conexion_api import download_station_data
+from src.process_ecobici_data import process_ecobici_data
+from src.create_aggregated_tables import process_station_data
+from src.upload_to_redshift import upload_files_to_redshift
+
 path = os.environ['AIRFLOW_HOME']
 
 default_args = {
-                'owner': 'airflow',
-                'depends_on_past': False,
-                'email_on_failure': False,
-                'email_on_retry': False,
-                'retries': 1,
-                'retry_delay': timedelta(minutes=5),
-                'start_date': datetime(2024, 9, 23),
-                'schedule_interval': '0 9 * * *', 
-                }
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2024, 10, 8),
+    'schedule_interval': '0 9 * * *', 
+}
 
 dag = DAG(
-            dag_id='ecobici_dag',
-            description='ETL para procesar datas de Ecobici Bs As',
-            default_args=default_args,
-            catchup=True,
-            max_active_tasks=32
-            )
+    dag_id='ecobici_dag',
+    description='ETL para procesar datas de Ecobici Bs As',
+    default_args=default_args,
+    catchup=True,
+    max_active_tasks=32
+)
 
-def conexion_api():
-    os.system(f'python {path}/dags/src/conexion_api.py')
-
-def process_ecobici_data():
-    os.system(f'python {path}/dags/src/process_ecobici_data.py')
-
-def create_aggregated_tables():
-    os.system(f'python {path}/dags/src/create_aggregated_tables.py')
-
-def upload_to_redshift():
-    os.system(f'python {path}/dags/src/upload_to_redshift.py')
-
+# Definir las tareas usando las funciones importadas
 task1 = PythonOperator(
-    task_id='conexion_api',
-    python_callable=conexion_api,
+    task_id='download_station_data',
+    python_callable=download_station_data,
     dag=dag
 )
 
@@ -49,16 +43,16 @@ task2 = PythonOperator(
 )
 
 task3 = PythonOperator(
-    task_id='create_aggregated_tables',
-    python_callable=create_aggregated_tables,
+    task_id='process_station_data',
+    python_callable=process_station_data,
     dag=dag
 )
 
 task4 = PythonOperator(
-    task_id='upload_to_redshift',
-    python_callable=upload_to_redshift,
+    task_id='upload_files_to_redshift',
+    python_callable=upload_files_to_redshift,
     dag=dag
 )
 
-
+# Definir las dependencias
 task1 >> task2 >> task3 >> task4
--- dags/src/upload_to_redshift.py
+++ dags/src/upload_to_redshift.py
@@ -2,64 +2,62 @@ import os
 import pandas as pd
 import redshift_connector
 from dotenv import dotenv_values
+import awswrangler as wr
 
-# Uso esta librería porque con sqlalchemy estaba teniendo problemas de compatibilidad con pandas pd.to_sql.
-import awswrangler as wr 
-
-
-# Verificar si AIRFLOW_HOME está definido, si no usa un path local
-if 'AIRFLOW_HOME' in os.environ:
-    path = os.environ['AIRFLOW_HOME']
-else:
-    # path = ./ecobici/
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-
-data_clean_dir = f'{path}/dags/data/clean/'
+# Cargar las credenciales desde el archivo .env
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 env_path = f'{path}/dags/env/redshift_key.env'
-
-# Leer credenciales
 credentials = dotenv_values(env_path)
 
-# Parámetros de conexión
+# Definir los parámetros de conexión
 conn_params = {
-  'host': 'redshift-pda-cluster.cnuimntownzt.us-east-2.redshift.amazonaws.com',
-  'database': 'pda',
-  'user': credentials['user'],
-  'password': credentials['password'],
-  'port': 5439,
+    'host': 'redshift-pda-cluster.cnuimntownzt.us-east-2.redshift.amazonaws.com',
+    'database': 'pda',
+    'user': credentials['user'],
+    'password': credentials['password'],
+    'port': 5439,
 }
 
-schema = f"{conn_params['user']}_schema"
+def get_connection():
+    """Establece y retorna la conexión a la base de datos Redshift."""
+    return redshift_connector.connect(**conn_params)
 
-# Conexión a la base de datos Redshift
-conn = redshift_connector.connect(**conn_params)
+def upload_to_redshift(file_name, table_name, mode, conn, schema):
+    """Sube un archivo CSV a Redshift."""
+    data_clean_dir = f'{os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))}/dags/data/clean/'
+    df = pd.read_csv(f'{data_clean_dir}/{file_name}')
+    wr.redshift.to_sql(
+        df=df,
+        con=conn,
+        table=table_name,
+        schema=schema,
+        mode=mode,
+        use_column_names=True,
+        lock=True,
+        index=False
+    )
 
-# Mapeo de archivos y tablas
-files_to_tables = {
-  'station_info_procesada.csv': 'stations_info',
-  'station_status_procesada.csv': 'stations_status',
-  'station_availability.csv': 'stations_availability',
-  'station_free_bikes.csv': 'stations_free_bikes',
-}
+def upload_files_to_redshift():
+    """Sube todos los archivos a las tablas correspondientes en Redshift."""
+    conn = get_connection()
+    schema = f"{conn_params['user']}_schema"
 
-# Función para subir a Redshift
-def upload_to_redshift(file_name, table_name, mode):
-  df = pd.read_csv(f'{data_clean_dir}/{file_name}')
-  wr.redshift.to_sql(
-    df=df,
-    con=conn,
-    table=table_name,
-    schema=schema,
-    mode=mode,
-    use_column_names=True,
-    lock=True,
-    index=False
-  )
+    files_to_tables = {
+        'station_info_procesada.csv': 'stations_info',
+        'station_status_procesada.csv': 'stations_status',
+        'station_availability.csv': 'stations_availability',
+        'station_free_bikes.csv': 'stations_free_bikes',
+    }
 
-# Subir archivos a tablas
-upload_to_redshift('station_info_procesada.csv', 'stations_info', 'overwrite') # stations_info es el único que debería sobreescribirse siempre con el SCD
-for file_name, table_name in files_to_tables.items():
-  if table_name != 'stations_info':
-    upload_to_redshift(file_name, table_name, 'append')
+    # Subir archivos a tablas
+    upload_to_redshift('station_info_procesada.csv', 'stations_info', 'overwrite', conn, schema)
+    for file_name, table_name in files_to_tables.items():
+        if table_name != 'stations_info':
+            upload_to_redshift(file_name, table_name, 'append', conn, schema)
 
-print("Todo subido a Redshift!! =)")
+    print("Todo subido a Redshift!! =)")
+    print(schema)
+
+if __name__ == '__main__':
+    upload_files_to_redshift()
+    
